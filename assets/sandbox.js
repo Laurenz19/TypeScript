fe6b6e679296728a6e3a71649417b3d525884ed1
Laurenz19 <laurenziosambany@gmail.com>
2022-05-23 18:47:53 +0300

    First commit

---
+++ assets/sandbox.js
@@ -0,0 +1,7 @@
+var character = "Laurenz";
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (element) {
+    console.log(element);
+});
---
+++ assets/sandbox.ts
@@ -0,0 +1,11 @@
+const character = "Laurenz";
+
+console.log(character);
+
+let inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach(element => {
+    console.log(element);
+});
